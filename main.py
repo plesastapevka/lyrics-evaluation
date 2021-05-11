2a6bfdbe90fe93f26444e69982f50c008d727291
Urban Vidovič <urbanfoundit@gmail.com>
2021-05-11 22:24:46 +0200

    Add test lyrics and precision tweaks

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import json
 
 
 def pre_process_text(text):
-    clean_text = re.sub(r'\[[^<]+]|((^|\s+)[me|im|you|and|in|yeah|i|ay|aye|at|to|the|a]+(\s+|$))', " ", text.lower())
+    # clean_text = re.sub(r"\[[^<]+]|((^|\s+)[ me | im | is | I'm | your | you | and | in | yeah | i | ay | aye | at | to | the | a]+(\s+|$))", " ", text.lower())
+    # clean_text = re.sub(r"\[[^<]+]", " ", text.lower())
+    clean_text = re.sub(r"\[[^<]+]|((^|\s+)[ yeah | ay | aye | uh | ugh | argh | na ]+(\s+|$))", " ", text.lower())
     raw_text = re.sub(r'[^A-Za-zČčŠšŽžĐđäöüßÄÖÜẞ. ]', '', clean_text)
     return raw_text
 
@@ -16,11 +18,16 @@ def create_n_grams(text, n=3):
     return [i[0] for i in ngrams_map]
 
 
-def build_model(path):
+def build_model(path, write=False):
     # Read lyrics from file
-    with open(path, 'r') as file:
-        data = file.read().replace('\n', ' ')
+    try:
+        infile = open(path, "r")
+        data = infile.read().replace('\n', ' ')
+    except IOError:
+        print("File does not exist")
+        exit(1)
 
+    infile.close()
     raw_text = pre_process_text(data)
     words_array = raw_text.split()
     unigram = list(ngrams(words_array, n=1))
@@ -41,10 +48,18 @@ def build_model(path):
         'fourgrams': dict(fourgram_freq[:300]),
         'fivegrams': dict(fivegram_freq[:300])
     }
-    with open("profile.json", "w") as outfile:
-        json.dump(profile, outfile)
-        print("Profile created.")
-        return
+    if write:
+        try:
+            outfile = open("learning_profiles/profile.json", "w")
+            json.dump(profile, outfile)
+            print("Profile created.")
+        except IOError:
+            print("Cannot write to file")
+            exit(1)
+        finally:
+            outfile.close()
+    else:
+        return profile
 
 
 def ngrams(s, n=2, i=0):
@@ -52,24 +67,6 @@ def ngrams(s, n=2, i=0):
         ret_val = s[i:i+n]
         yield " ".join(ret_val)
         i += 1
-
-
-def get_ngrams(path_name="", string=False, input_text=""):
-    if not string:
-        text = open(path_name).read()
-        raw_text = pre_process_text(text)
-        bigrams = create_n_grams(raw_text, 2)
-        trigrams = create_n_grams(raw_text, 3)
-        fourgrams = create_n_grams(raw_text, 4)
-        fivegrams = create_n_grams(raw_text, 5)
-    else:
-        raw_text = pre_process_text(input_text)
-        bigrams = create_n_grams(raw_text, 2)
-        trigrams = create_n_grams(raw_text, 3)
-        fourgrams = create_n_grams(raw_text, 4)
-        fivegrams = create_n_grams(raw_text, 5)
-
-    return bigrams, trigrams, fourgrams, fivegrams
 
 
 def get_diff(ngram, element, idx):
@@ -80,81 +77,76 @@ def get_diff(ngram, element, idx):
         return len(ngram)
 
 
-def detect_lang(bi, tri, four, five):
-    bi_si, tri_si, four_si, five_si = get_ngrams("models/si.txt")
-    bi_eng, tri_eng, four_eng, five_eng = get_ngrams("models/eng.txt")
-    bi_ger, tri_ger, four_ger, five_ger = get_ngrams("models/ger.txt")
-    sim_sum_si = 0
-    sim_sum_eng = 0
-    sim_sum_ger = 0
+def process_ngram(new_ngram, ngram):
+    ngram_sum = 0
+    for idx, e in enumerate(new_ngram):
+        diff = get_diff(ngram, e, idx)
+        if diff != -1:
+            ngram_sum += diff
+    return ngram_sum
 
-    # bigram processing
-    for idx, e in enumerate(bi):
-        diff_si = get_diff(bi_si, e, idx)
-        diff_eng = get_diff(bi_eng, e, idx)
-        diff_ger = get_diff(bi_ger, e, idx)
-        if diff_si != -1:
-            sim_sum_si += diff_si
-        if diff_eng != -1:
-            sim_sum_eng += diff_eng
-        if diff_ger != -1:
-            sim_sum_ger += diff_ger
 
-    # trigram processing
-    for idx, e in enumerate(tri):
-        diff_si = get_diff(tri_si, e, idx)
-        diff_eng = get_diff(tri_eng, e, idx)
-        diff_ger = get_diff(tri_ger, e, idx)
-        if diff_si != -1:
-            sim_sum_si += diff_si
-        if diff_eng != -1:
-            sim_sum_eng += diff_eng
-        if diff_ger != -1:
-            sim_sum_ger += diff_ger
+def calculate(new_profile, path="learning_profiles/profile.json"):
+    try:
+        infile = open(path, "r")
+        profile = json.load(infile)
+    except IOError:
+        print("Cannot open file")
+        exit(1)
 
-    # fourgram processing
-    for idx, e in enumerate(four):
-        diff_si = get_diff(four_si, e, idx)
-        diff_eng = get_diff(four_eng, e, idx)
-        diff_ger = get_diff(four_ger, e, idx)
-        if diff_si != -1:
-            sim_sum_si += diff_si
-        if diff_eng != -1:
-            sim_sum_eng += diff_eng
-        if diff_ger != -1:
-            sim_sum_ger += diff_ger
+    infile.close()
 
-    # fivegram processing
-    # for idx, e in enumerate(five):
-    #     diff_si = get_diff(five_si, e, idx)
-    #     diff_eng = get_diff(five_eng, e, idx)
-    #     diff_ger = get_diff(five_ger, e, idx)
-    #     if diff_si != -1:
-    #         sim_sum_si += diff_si
-    #     if diff_eng != -1:
-    #         sim_sum_eng += diff_eng
-    #     if diff_ger != -1:
-    #         sim_sum_ger += diff_ger
+    # test profiles
+    uni_new = list(new_profile["unigrams"].keys())
+    bi_new = list(new_profile["bigrams"].keys())
+    tri_new = list(new_profile["trigrams"].keys())
+    four_new = list(new_profile["fourgrams"].keys())
+    five_new = list(new_profile["fivegrams"].keys())
 
-    # print("English similarity sum is: " + str(sim_sum_eng))
-    # print("Slovene similarity sum is: " + str(sim_sum_si))
-    # print("German similarity sum is: " + str(sim_sum_ger))
-    if min([sim_sum_si, sim_sum_eng, sim_sum_ger]) == sim_sum_eng:
-        return "english"
-    elif min([sim_sum_si, sim_sum_eng, sim_sum_ger]) == sim_sum_si:
-        return "slovenian"
-    elif min([sim_sum_si, sim_sum_eng, sim_sum_ger]) == sim_sum_ger:
-        return "german"
+    # learning profiles
+    uni = list(profile["unigrams"].keys())
+    bi = list(profile["bigrams"].keys())
+    tri = list(profile["trigrams"].keys())
+    four = list(profile["fourgrams"].keys())
+    five = list(profile["fivegrams"].keys())
+
+    ngram_sum = 0
+
+    # ngrams processing
+    # ngram_sum += process_ngram(uni_new, uni)
+    ngram_sum += process_ngram(bi_new, bi)
+    ngram_sum += process_ngram(tri_new, tri)
+    ngram_sum += process_ngram(four_new, four)
+    ngram_sum += process_ngram(five_new, five)
+
+    return ngram_sum
 
 
 def main():
-    create_model = True
+    create_model = False
     if create_model:
-        build_model(path="hiphoplyrics.txt")
+        build_model(path="learning_profiles/hiphoplyrics.txt", write=True)
     else:
-        bigrams, trigrams, fourgrams, fivegrams = get_ngrams("korpus/kas-5000.text.xml")
-        print("Detecting language ...")
-        detect_lang(bigrams, trigrams, fourgrams, fivegrams)
+        profile = build_model(path="test_lyrics/jcole_middle_child.txt")
+        print("J. Cole - Middle Child: " + str(calculate(new_profile=profile)))
+
+        profile = build_model(path="test_lyrics/eminem_kim.txt")
+        print("Eminem - Kim: " + str(calculate(new_profile=profile)))
+
+        profile = build_model(path="test_lyrics/melvoni_no_mans_land.txt")
+        print("Melvoni - No Man's Land: " + str(calculate(new_profile=profile)))
+
+        profile = build_model(path="test_lyrics/kid_cudi_day_n_nite.txt")
+        print("Kid Cudi - Day 'n' Nite: " + str(calculate(new_profile=profile)))
+
+        profile = build_model(path="test_lyrics/mobb_deep_shook_ones.txt")
+        print("Mobb Deep - Shook Ones, Part II: " + str(calculate(new_profile=profile)))
+
+        profile = build_model(path="test_lyrics/passenger_let_her_go.txt")
+        print("Passenger - Let Her Go: " + str(calculate(new_profile=profile)))
+
+        profile = build_model(path="test_lyrics/gnr_sweet_child.txt")
+        print("Guns N' Roses - Sweet Child O' Mine: " + str(calculate(new_profile=profile)))
 
 
 if __name__ == "__main__":
